25ea18553c3f8336d9f09e8c62be5f152525df39
dart-suite <dart@doosan.com>
2023-06-16 18:46:14 +0900

    Add more gripper data (Update v1.1.0)

--- client_save_code.py
+++ client_save_code.py
@@ -1,12 +1,18 @@
 # -------------------------------------------------------------------------
-# BSD 3-Clause License
 # Copyright (c) 2023 Doosan Robotics
 # All rights reserved.
 # -------------------------------------------------------------------------
 # Title: Robotiq gripper test client code using socket communication.
 # Date: Apr, 20, 2023
+# Updated: Jun, 16, 2023
+# Name: Seonghyeon Jo
+# Email: seonghyeon2.jo@doosan.com
 # -------------------------------------------------------------------------
-# Note, you need to run the DRL server code first in dart-studio and then run it.
+# Note, 
+# you need to run the DRL server code first in dart-studio and then run it.
+# If you want another gripper data, 
+# change the file header in .py and the global variable name in .drl.
+# -------------------------------------------------------------------------
 
 # Import the python library.
 import socket
@@ -16,6 +22,9 @@ import datetime
 # Define the host and port to connect to
 HOST = '192.168.137.100'    # Replace with the server's hostname or IP address
 PORT = 20002                # Replace with the server's port number
+FILE_TYPE = ".txt"          # Change it to the desired file type.
+# Change it to the desired file header
+FILE_HEADER = "TIME, state, prev position, actual position, current" +"\n"
 
 # Create a socket object and connect to the server
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -27,12 +36,11 @@ current_date = datetime.datetime.now()
 formatted_date = current_date.strftime(date_format)[:-3]
 
 # It performs socket communication with the server and saves the state data of the gripper.
-filename = "sample_data_" + formatted_date + ".txt"
+filename = "gripper_test_" + formatted_date + FILE_TYPE
 with open(filename, 'w') as f:
 
     # Sets the header of the file.
-    file_header = "time, state, current\n"
-    f.write(file_header)
+    f.write(FILE_HEADER)
 
     # Receive data from the server line by line and save it to the file
     while True:
